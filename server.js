edea73f21608b05556c0dfa05252c9c38fc70804
shoaib Hussain <107783902+Shoaib7457@users.noreply.github.com>
2024-10-02 20:04:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,36 +5,40 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
-app.use(cors());
-
+const PORT = process.env.PORT || 5000;
+app.use(cors({
+    origin: 'https://front-end-pi-gules.vercel.app/', // allow requests from your frontend
+    credentials: true
+}));
 // // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'vehicle-backend/build')));
+app.use(express.static(path.join(__dirname, 'maps/build')));
 
 // // Serve React app for any unhandled routes
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'vechicle/build', 'index.html'));
-// });
-
-app.get('/api/vehicle', (req, res) => {
-  const dataPath = path.join('data', 'vehicledata.json');
-  
-  fs.readFile(dataPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading vehicle data:', err);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    // Parse and send the data as JSON
-    const vehicleData = JSON.parse(data);
-    res.json(vehicleData);
-  });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'maps/build', 'index.html'));
 });
 
-let currentIndex = 0;
+// app.get('/api/vehicle', (req, res) => {
+//   const dataPath = path.join('data', 'vehicledata.json');
+  
+//   fs.readFile(dataPath, 'utf8', (err, data) => {
+//     if (err) {
+//       console.error('Error reading vehicle data:', err);
+//       return res.status(500).json({ error: 'Internal Server Error' });
+//     }
 
+//     // Parse and send the data as JSON
+//     const vehicleData = JSON.parse(data);
+//     res.json(vehicleData);
+//   });
+// });
+
+let currentIndex = 0;
+app.get("/" , (req , res) =>{
+  let currentIndex = 0;
+});
 app.get('/api/vehicle/current', (req, res) => {
-  const dataPath = path.join(__dirname, 'data', 'vehicleData.json');
+  const dataPath = path.join(__dirname, 'data', 'vehicledata.json');
 
   fs.readFile(dataPath, 'utf8', (err, data) => {
     if (err) {
@@ -57,6 +61,6 @@ app.get('/api/vehicle/current', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
